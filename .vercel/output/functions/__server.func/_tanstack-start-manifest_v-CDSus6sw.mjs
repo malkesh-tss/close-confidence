0662e275dc9070c7905f3de7e7724a2e35b873f0
malkesh-tss <malkesh@thescalesummit.com>
2026-05-25 15:34:53 -0700

    remove the versel  things

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-CDSus6sw.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "D:/Darrin Guttman/Landing_page/close-confidence/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-DhURZHly.js"] }, "/": { filePath: "D:/Darrin Guttman/Landing_page/close-confidence/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-BS1DSY52.js"] } }, clientEntry: "/assets/index-DhURZHly.js" });
-export {
-  tsrStartManifest
-};
